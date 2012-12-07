cc261f21922e99f7f9da5ea8500bc3560f7ae8c6
Felix Clack <felixclack@gmail.com>
2012-12-07 15:08:38 +0000

    Require stuff so the gem works
    Also add a convenience ref for accessing the modules.
    eg. LRel::Relation.

--- lib/ldap-relations.rb
+++ lib/ldap-relations.rb
@@ -1,6 +1,10 @@
 require "ldap-relations/version"
+require 'ldap-relations/relation_manager'
+require 'ldap-relations/relation'
 
 module Ldap
   module Relations
   end
 end
+
+LRel = Ldap::Relations
